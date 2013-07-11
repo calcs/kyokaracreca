722a74e9bdb265aab0050d1b8d32d1d2dc3429e6
calcs <calcs.tacram+github@gmail.com>
2013-07-11 11:59:33 +0900

    first release

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+  if (!defined('APP_PATH')) exit;
+
+  /**
+  * 設定ファイル
+  */
+  
+  $config = array(
+    'force_ssl' => false,  //非SSLで接続した時に強制的にSSL接続にするオプション。本番時はtrueを強く推奨
+    'disable_iframe' => true, //クリックジャッキング対策として、iframeで決済画面が読み込めないようにするオプション。trueを強く推奨
+  
+    'do_notify' => true,  //メールアドレスに決済の通知をするか否か
+    'notify_mail' => 'yamamoto+kcc@s0cial-design.com',  //決済時の通知先メールアドレス
+    'notify_subject' => '決済が有りました', //通知メールの件名
+    'notify_from' => 'test@example.com',
+    'page_title' => 'サンプル決済', //ページのタイトル
+    'back_url' => 'http://www.example.com/',  //「戻る」をクリックした時の遷移先
+    'min_amount' => 50, //最低の決済額（円）。50円だと、決済手数料で赤字になることはない2013年7月10日
+    'max_amount' => 0, //最大の決済額。これを超える決済はできない。0で無制限
+    'default_amount' => 100,  //GET引数による値段引渡しを使わない場合の標準価格
+    'fixed_amount' => false, //固定金額決済モード。default_amountから、GET引数でもPOSTでも変更できなくなる。
+    
+    'webpay_secret_key' => 'test_secret_eHn4TTgsGguBcW764a2KA8Yd',
+  );
---
+++ index.php
@@ -0,0 +1,137 @@
+<?php
+  define('APP_PATH', __DIR__ . '/');
+  include(APP_PATH . 'config.php');
+  
+  if ($config['force_ssl'] === true && empty($_SERVER['HTTPS'])) {
+    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '?' . $_SERVER['QUERY_STRING']);
+    exit;
+  }
+  
+  if ($config['disable_iframe'] === true) {
+    header('X-FRAME-OPTIONS: DENY');
+  }
+  
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = validate();
+  }
+  
+  if ($_SERVER['REQUEST_METHOD'] === 'GET' || count($errors) > 0 || isset($_POST['back'])) {
+    include 'tmpl/index.html';
+  } else {
+    if (isset($_POST['confirm'])) {
+      include 'tmpl/confirm.html';
+    } else {
+      require_once("./lib/Stripe.php");
+      Stripe::setApiKey($config['webpay_secret_key']);
+      Stripe::$apiBase = "https://api.webpay.jp";
+      
+      $result = Stripe_Charge::create(array(
+         "amount" => $_POST['amount'],
+         "currency" => "jpy",
+         "card" => 
+          array("number" => $_POST['card_num'],
+           "exp_month" => $_POST['exp_month'],
+           "exp_year" => $_POST['exp_year'],
+           "cvc" => $_POST['cvc'],
+           "name" => $_POST['card_name']),
+           "description" => isset($_GET['m'])?$_GET['m']:null
+      ));
+      
+      if ($config['do_notify'] === true) {
+        $body = "{$config['page_title']}にて決済が有りました。\n日時: " . date('Y-m-d H:i:s') . "\n金額: {$_POST['amount']}\n\n詳しくはWebpayの管理画面で確認ください。";
+      
+        mb_send_mail($config['notify_mail'], $config['notify_subject'], $body, 'Form: ' . $config['notify_from']);
+      }
+      
+      include 'tmpl/complete.html';
+    }
+  }
+  
+  function validate() {
+    $errors = array();
+  
+    $_POST['card_num'] = trim(str_replace(array('-', '－', 'ー', ' ', '　'), '', mb_convert_kana($_POST['card_num'], 'sa')));
+    $_POST['exp_month'] = (int)trim($_POST['exp_month']);
+    $_POST['exp_year'] = (int)trim($_POST['exp_year']);
+    $_POST['cvc'] = trim(mb_convert_kana($_POST['cvc'], 'sa'));
+    $_POST['card_name'] = trim(mb_convert_kana($_POST['card_name'], 'sa'));
+    $_POST['amount'] = trim(str_replace(array(',', '，'), '', mb_convert_kana($_POST['amount'], 'sa')));
+    
+    if (strlen($_POST['card_num']) < 13 || strlen($_POST['card_num']) > 16) {
+      $errors['card_num'] = 'カード番号の桁数が不正です';
+    } elseif (!ctype_digit($_POST['card_num'])) {
+      $errors['card_num'] = 'カード番号は数字で入力してください';
+    } elseif (!check_by_luhn($_POST['card_num'])) {
+      $errors['card_num'] = 'カード番号が不正です';
+    }
+    
+    if (strlen($_POST['cvc']) !== 3 && strlen($_POST['cvc']) !== 4) {
+      $errors['cvc'] = 'CVCの桁数が不正です';
+    } elseif (!ctype_digit($_POST['cvc'])) {
+      $errors['cvc'] = 'CVCは数字で入力してください';
+    }
+    
+    if (count(explode(' ', $_POST['card_name'])) !== 2) {
+      $errors['card_name'] = 'カード名義人名はスペースで区切られている必要があります';
+    }
+    
+    if ($_POST['exp_month'] < 1 || $_POST['exp_month'] > 12) {
+      $errors['exp'] = '有効期限が不正です';
+    } elseif ($_POST['exp_year'] < date('Y') || $_POST['exp_year'] > (date('Y') + 10)) {  //この10は11年以上先まで有効期限を持つカードはないだろうという見込みで書かれている。Magig Number
+      $errors['exp'] = '有効期限が不正です';
+    } elseif (date('Ym') > (int)($_POST['exp_year'] . sprintf('%02d', $_POST['exp_month']))) {
+      $errors['exp'] = '有効期限が切れています';
+    }
+    
+    global $config;
+    if ($config['fixed_amount'] === true) {
+      $_POST['amount'] = $config['default_amount'];
+    }
+    
+    if (!ctype_digit($_POST['amount'])) {
+      $errors['amount'] = '決済金額は整数の数字で入力してください。';
+    } elseif ($_POST['amount'] < $config['min_amount']) {
+      $errors['amount'] = '決済金額が最低金額(' . $config['min_amount'] . '円)を下回っています。';
+    } elseif ($config['max_amount'] !== 0 && $_POST['amount'] > $config['max_amount']) {
+      $errors['amount'] = '決済金額が最大金額(' . $config['max_amount'] . '円)を上回っています。';
+    }
+    
+    return $errors;
+  }
+  
+  function h($s) {
+    return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+  }
+  
+  /**
+  * Luhn Algorithm(Mod 10)による、カード番号検査
+  */
+  function check_by_luhn($number)
+  {
+    //桁数によるparityの判定
+    $parity = strlen($number) % 2;
+
+    $total = 0;
+    for ($i = 0; $i < strlen($number); $i++) {
+      $digit = $number[$i];
+      
+      if ($i % 2 == $parity) {
+        $digit *= 2;
+        if ($digit > 9) {
+          $digit -= 9;
+        }
+      }
+      $total += $digit;
+    }
+
+    return (($total % 10) === 0) ? true : false;
+  }
+  
+  function trunc_credit_card_num($str) {
+    $buf = $str;
+  
+    for($i = 0; $i < strlen($buf)-4; $i++) {
+      $buf[$i] = '*';
+    }
+    return str_replace(['-', '－'], '', $buf);
+  }
